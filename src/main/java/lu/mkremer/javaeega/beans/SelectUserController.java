4323d7c16c5b9d944499e850951f030c637a2e74
Michel Kremer <kremi151dev@gmail.com>
2018-01-04 12:15:57 +0100

    Some useless import organization

--- src/main/java/lu/mkremer/javaeega/beans/SelectUserController.java
+++ src/main/java/lu/mkremer/javaeega/beans/SelectUserController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.event.ActionEvent;
 
 import lu.mkremer.javaeega.managers.UserManager;
 import lu.mkremer.javaeega.users.User;
